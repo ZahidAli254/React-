2a2b81985d33c1bfbe90654494da58eaf1c1b25e
ZahidCodes <zahidheir1234@gmail.com>
2023-04-11 16:37:27 +0530

    ReactComponent

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,9 @@
+function ExpenseItem (){
+    return( <div><h2>Expense Item!</h2>
+    <div>Food Rs100</div>
+    <div>Petrol Rs100</div>
+    <div>Movie Rs100</div>
+    </div>
+    )
+}
+export default ExpenseItem;
