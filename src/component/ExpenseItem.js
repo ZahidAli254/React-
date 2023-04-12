bc8142b378fe793c4f6f4bce1192f63f44d733bc
ZahidCodes <zahidheir1234@gmail.com>
2023-04-12 22:36:14 +0530

    add fat arrow and Card component

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,14 +1,17 @@
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
+import Card from './UI/Card';
 
 function ExpenseItem (props){
      return( 
-    <div className='expense-item'>
+    <Card className='expense-item'>
         <ExpenseDate date={props.Data.date}></ExpenseDate> 
-<ExpenseDetails amount={props.Data.Amount}
-title={props.Data.title} LocationOfExpenditure={props.Data.LocationOfExpenditure}></ExpenseDetails>
-    </div>
+        <ExpenseDetails amount={props.Data.amount}
+                        title={props.Data.title} 
+                        LocationOfExpenditure={props.Data.LocationOfExpenditure}
+                        ></ExpenseDetails>
+    </Card>
     );
 }
 export default ExpenseItem;
